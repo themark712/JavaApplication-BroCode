b5f2931b3d868a8c2da724bdc4bdf298c0e1c63d
themark712 <themark712@yahoo.com>
2025-10-06 10:40:36 -0500

    test 2

--- src/javaapplication/Main.java
+++ src/javaapplication/Main.java
@@ -8,11 +8,27 @@ public class Main {
     // if statement = performs a block of code if its condition is true
     Scanner scanner = new Scanner(System.in);
 
+    String name;
     int age;
+    boolean isFan;
     
+    System.out.print("What is your name?: ");
+    name = scanner.nextLine();
+
     System.out.print("What is your age?: ");
     age = scanner.nextInt();
 
+    System.out.print("Are you a Hilltopper fan?: ");
+    isFan = scanner.nextBoolean();
+
+    // group 1
+    if(name.isEmpty()) {
+      System.out.println("You didn't enter your name!");
+    } else {
+      System.out.println("Hello, " + name + "!");
+    }
+    
+    // group 2
     if (age >= 65) {
       System.out.println("You are a senior!");
     }
@@ -27,6 +43,13 @@ public class Main {
     }
     else {
       System.out.println("You are a child!");
+    }
+    
+    // group 3
+    if(isFan) {
+      System.out.println("You are a fan!");
+    } else {
+      System.out.println("Boooooo!");
     }
 
     scanner.close();
