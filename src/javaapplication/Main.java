7ce1c145193fd1017399b1147320b84d8b6c7f1a
themark712 <themark712@yahoo.com>
2025-10-06 12:43:36 -0500

    test 2

--- src/javaapplication/Main.java
+++ src/javaapplication/Main.java
@@ -1,96 +1,84 @@
 /*
-  Java Math class
+  printf() function
  */
 package javaapplication;
 
-import java.util.Scanner;
-
 public class Main {
-  
   public static void main(String[] args) {
-    // constants
-    // PI
-    System.out.println(Math.PI);
+    // printf() is a method used to format output
+    // printf(%[flags][width][.precision][specifier-character]
+    //   specifier-character :
+    //     String  : s
+    //     char    : c
+    //     int     : d
+    //     double  : f
+    //     char    : c
+    //     boolean : b
     
-    // Exponential constant (Euler's number)
-    System.out.println(Math.E);
+    String name = "Homer Simpson";
+    char firstLetter = 'H';
+    int age = 43;
+    double height = 72.5;
+    boolean isEmployed = true;
     
-    // Methods
-    double result;
+    // specifier-character
+    System.out.printf("Hello %s\n", name);
+    System.out.printf("Your name starts with a(n) %c\n", firstLetter);
+    System.out.printf("You are %d years old\n", age);
+    System.out.printf("You are %f inches tall\n", height);
+    System.out.printf("Employed: %b\n", isEmployed);
     
-    // Power (Math.pow(base, power)) function: returns base raised to power of power
-    result = Math.pow(2, 3);
-    System.out.println(result);
+    // displaying multiple variables
+    System.out.printf("%s is %d years old.\n", name, age);
     
-    // Absolute value (Math.abs(value)): returns absolute value of value
-    result = Math.abs(-900);
-    System.out.println(result);
+    // precision
+    double price1 = 9.9997;
+    double price2 = 10019.150392;
+    double price3 = -54.0154;
     
-    // Square root (Math.sqrt(value)) : returns the square root of value
-    result = Math.sqrt(999);
-    System.out.println(result);
+    System.out.printf("%f\n", price1);   // default precision is 6 digits
+    System.out.printf("%f\n", price2);
+    System.out.printf("%f\n", price3);
     
-    // Rounding (Math.round(value)) : returns value rounded to nearest integer
-    result = Math.round(3.14);
-    System.out.println(result);
-    result = Math.round(3.5);
-    System.out.println(result);
+    System.out.printf("%.1f\n", price1);   // precision of 1 digit
+    System.out.printf("%.2f\n", price2);   // precision of 2 digits
+    System.out.printf("%.3f\n", price3);   // precision of 3 digits
     
-    // Rounding up (Math.ceil(value)) : returns value rounded to next highest integer
-    result = Math.ceil(3.14);
-    System.out.println(result);
+    // flags
+    // +     : outputs a plus sign for positive numbers
+    // ,     : comma grouping seperator
+    // (     : negative numbers are enclosed in ()
+    // space : display a minus if negative, space if positive
     
-    // Rounding down (Math.floor(value)) : returns value rounded to next lowest integer
-    result = Math.floor(3.99);
-    System.out.println(result);
+    System.out.printf("%+.2f\n", price1);
+    System.out.printf("%,.2f\n", price2);
+    System.out.printf("%(.2f\n", price3);
+    System.out.printf("% .2f\n", price2);
+    System.out.printf("% .2f\n", price3);
     
-    // Maximum of two values (Math.max(value1, value2) : returns the higher of value1 and value2
-    result = Math.max(1.221, 1.222);
-    System.out.println(result);
+    // width
+    int id1 = 1;
+    int id2 = 23;
+    int id3 = 475;
+    int id4 = 9591;
     
-    // Minimum of two values (Math.max(value1, value2) : returns the lower of value1 and value2
-    result = Math.max(-1.221, 1.2202);
-    System.out.println(result);
+    // zero-padded to width of 4
+    System.out.printf("%04d\n", id1);
+    System.out.printf("%04d\n", id2);
+    System.out.printf("%04d\n", id3);
+    System.out.printf("%04d\n", id4);
     
-    Scanner scanner = new Scanner(System.in);
-    double sideA;
-    double sideB;
-    double sideC;
+    // space-padded to width of 4, right-justified
+    System.out.printf("%4d\n", id1);
+    System.out.printf("%4d\n", id2);
+    System.out.printf("%4d\n", id3);
+    System.out.printf("%4d\n", id4);
     
-    // Exercises
-    // Hypotenuse : c = Math.sqrt(a^2 + b^2)
-    System.out.print("Input the length of side a: ");
-    sideA = scanner.nextDouble();
-    
-    System.out.print("Input the length of side b: ");
-    sideB = scanner.nextDouble();
-    
-    sideC = Math.sqrt((Math.pow(sideA, 2) + Math.pow(sideB, 2)));
-    System.out.println("Hypotenuse = " + sideC);
-    
-    // Circumference (circle), Area (circule), Volume (sphere)
-    // circumference = 2 * Math.PI * radius
-    // area = Math.PI * Math.pow(radius, 2)
-    // volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3)
-    
-    double radius;
-    double circumference;
-    double area;
-    double volume;
-    
-    System.out.print("Input the radius: ");
-    radius = scanner.nextDouble();
-    
-    circumference = radius * Math.PI * 2;
-    area = Math.pow(radius,2) * Math.PI;
-    volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
-    
-    // display with desired digits with printf
-    System.out.printf("The circumference is: %.2f\n", circumference);
-    System.out.printf("The area is: %.2f\n", area);
-    System.out.printf("The volume is: %.2f\n", volume);
-    
-    scanner.close();
+    // space-padded to width of 4, left-justified
+    System.out.printf("%-4d\n", id1);
+    System.out.printf("%-4d\n", id2);
+    System.out.printf("%-4d\n", id3);
+    System.out.printf("%-4d\n", id4);
   }
   
 }
